154351be166b1924bc004d8025039dcb72d1740d
janburian <jburian@students.zcu.cz>
2023-04-19 16:36:17 +0200

    Working better

--- classifier.py
+++ classifier.py
@@ -3,11 +3,11 @@ import urllib
 import skimage
 import skimage.color
 import skimage.measure
-# import skimage.ioconda
 import numpy as np
 from sklearn import svm
 from matplotlib import pyplot as plt
 from sklearn import neighbors
+from skimage.feature import corner_fast, corner_peaks
 
 def count_object_area(object):
     number_white_pixels = 0
@@ -21,7 +21,7 @@ def count_object_area(object):
 
 def count_perimeter(object):
     object_int = object.astype(int)
-    perimeter = skimage.measure.perimeter(object_int, 8)
+    perimeter = skimage.measure.perimeter(object_int, 4)
 
     return perimeter
 
@@ -39,7 +39,15 @@ def count_object_features(object):
     area = count_object_area(object)
     perimeter = count_perimeter(object)
     incompatibility = (perimeter ** 2) / area
+    # corners = corner_fast(object, n=10, threshold=0)
+    # number_corners = len(corner_peaks(corners))
+    # plt.imshow(corners)
+    # plt.show()
+    # features = [incompatibility, area, perimeter]
     features = [incompatibility]
+    # plt.imshow(object)
+    # plt.show()
+
     return features
 
 
@@ -90,13 +98,15 @@ def get_features_test_data(test_data):
 
     return np.array(object_features)
 
-def predict_data(knn, features_test_data, filenames):
+def predict_data(knn, features_test_data, filenames, classes_str):
     index = 0
     for features in features_test_data:
         predictions = []
         for feature in features:
             prediction = knn.predict([feature])
-            predictions.append(prediction)
+            class_index = prediction[0]
+            object = classes_str[class_index]
+            predictions.append(object)
         print(filenames[index] + ": " + str(predictions))
         index += 1
 
@@ -111,6 +121,8 @@ if __name__ == "__main__":
     plt.imshow(img_labeled, cmap='gray')
     plt.show()
 
+    test = skimage.measure.regionprops(img_labeled)
+
     # Extracting objects (0 = background)
     squares = [img_labeled == 1, img_labeled == 2, img_labeled == 3]
     stars = [img_labeled == 4, img_labeled == 5, img_labeled == 6]
@@ -118,6 +130,7 @@ if __name__ == "__main__":
 
     # Classes (0 = square, 1 = star, 2 = circle)
     classes = np.array([squares, stars, circles])
+    classes_str = ["square", "star", "circle"]
 
     # Training data
     train_data = get_train_data(classes)
@@ -135,7 +148,6 @@ if __name__ == "__main__":
                  "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg"]
     test_data = load_test_data(test_data_url, filenames)
     features_test_data = get_features_test_data(test_data)
-    predict_data(knn, features_test_data, filenames)
-    print()
+    predict_data(knn, features_test_data, filenames, classes_str)
 
 
